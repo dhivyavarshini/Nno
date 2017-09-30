84556db7e36d4d98d37354a47ac6fb31b7d9b8e7
dhivyavarshini <31499146+dhivyavarshini@users.noreply.github.com>
2017-09-30 15:38:03 +0530

    Create Nno.c

---
+++ Nno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,s=0 i;
+printf("enter the number");
+scanf("%d",&N);
+for(i=1;i<=N;++i)
+{
+s=s+1;
+}
+printf("sum of N numbers=%d,s);
+getch();
+}
